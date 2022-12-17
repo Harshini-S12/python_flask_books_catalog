84af88f84c649c08ee78593a89244cde177d9277
Harshini-S12 <bharshinis@gmail.com>
2022-12-17 22:43:05 +0530

    Updated files

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 
 # dev,test, prod
@@ -14,9 +16,11 @@ def create_app(config_type):
     # 'C:\\Users\\Admin\\PycharmProjects\\book_catalog\\config\\dev.py'
     app.config.from_pyfile(configuration)
 
-    db.init_app(app)
+    db.init_app(app) # binds db to flask app
+    bootstrap.init_app(app) # initializes bootstrap
+    from app.catalog import main # importing blueprint
+    app.register_blueprint(main) # registering the blueprint
 
-    from app.catalog import main
-    app.register_blueprint(main)
-
+    #from app.auth import authentication as at # importing blueprint
+    #app.register_blueprint(at) #registering blueprint
     return app
