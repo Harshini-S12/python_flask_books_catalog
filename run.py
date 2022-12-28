42c5de476bde3d7743ad30d657f752a9a5996bd9
Harshini-S12 <bharshinis@gmail.com>
2022-12-29 00:31:26 +0530

    final changes

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import create_app,db
 from app.auth.models import User
 
 
-flask_app = create_app('dev')
+flask_app = create_app('prod')
 with flask_app.app_context():
 
     db.create_all()
