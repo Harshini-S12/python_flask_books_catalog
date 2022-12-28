1c38ed7dc91fe7ec4d3dca02901e8fee88516779
Harshini-S12 <bharshinis@gmail.com>
2022-12-29 00:49:32 +0530

    modified some files

--- app/catalog/forms.py
+++ app/catalog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, FloatField, FileField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+from app.catalog.models import Publication
 
 
 class EditBookForm(FlaskForm):
@@ -21,7 +23,12 @@ class CreateBookForm(FlaskForm):
     pub_id = IntegerField('Publisher ID', validators=[DataRequired()])
     submit = SubmitField('Create')
 
+def publisher_exists(form,field):
+    email = Publication.query.filter_by(name=field.data).first()
+
+    if email:
+        raise ValidationError('Publisher Already Exists')
 class CreatePublisherForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), publisher_exists])
     id = IntegerField('Publisher ID', validators=[DataRequired()])
     submit = SubmitField('Create')
