6f0182da223a4b3b912fc8dd1037d58f7778a9ef
Harshini-S12 <bharshinis@gmail.com>
2022-12-28 21:28:26 +0530

    config changes

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    FLASK_ENV = 'development'
+    FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
